6a164c479daf2bb85a9b3dbfadce85f25d7e4f0d
hirobon1690 <hirobon1690@gmail.com>
2024-09-10 06:29:15 +0900

    update submodule

---
+++ catch2024_teamr/catch2024_teamr/config/mode.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+class Mode(IntEnum):
+    DEFAULT = 0
+    YURAYURA = 1
+    GURIGURI = 2
+    PATAPATA = 3
--- catch2024_teamr/catch2024_teamr/seiton/full_manual.py
+++ catch2024_teamr/catch2024_teamr/seiton/full_manual.py
@@ -1,10 +1,11 @@
 import rclpy
 from rclpy.node import Node
-from catch2024_teamr_msgs.msg import MainArm
+from catch2024_teamr_msgs.msg import Seiton
 from sensor_msgs.msg import Joy
 import math
 from enum import IntEnum
 from ..config.joy import *
+from ..config.mode import Mode
 
 class FullManual(Node):
     def __init__(self):
@@ -13,13 +14,18 @@ class FullManual(Node):
         self.joy_sub = self.create_subscription(
             Joy, '/seiton/joy', self.joy_callback, 5)
         self.get_logger().info('full_manual has been started')
-        self.mainarm_msg = MainArm()
+        self.seiton_msg = Seiton()
         self.tmr = self.create_timer(0.01, self.timer_callback)
+
         self.joy_msg = Joy()
         self.joy_msg.axes = [0.0]*8
-        self.joy_msg.buttons = [0]*11
+        self.joy_msg.buttons = [0]*12
         self.previous_joy_msg = self.joy_msg
-        self.coordinate_mode = CoordinateMode.POLAR
+
+        self.seiton_msg.y = 0.0
+        self.seiton_msg.mode = 0
+        self.seiton_msg.conveyer = 0
+        self.seiton_msg.flip = False
 
         self.cartesian_xy = [0, 0]
 
@@ -27,79 +33,44 @@ class FullManual(Node):
         self.joy_msg = msg
 
     def timer_callback(self):
-        if self.coordinate_mode == CoordinateMode.POLAR:
-            self.mainarm_msg.theta += self.joy_msg.axes[Axes.LX]/100
-            if self.mainarm_msg.theta < -math.pi/2:
-                self.mainarm_msg.theta = -math.pi/2
-            elif self.mainarm_msg.theta > math.pi*3/2:
-                self.mainarm_msg.theta = math.pi*3/2
+        if self.joy_msg.buttons[0]:
+            self.y += 0.05
 
-            self.mainarm_msg.r += self.joy_msg.axes[Axes.LY]/100
-            if self.mainarm_msg.r < 0.0:
-                self.mainarm_msg.r = 0.0
-            elif self.mainarm_msg.r > 1.0:
-                self.mainarm_msg.r = 1.0
+        if self.joy_msg.buttons[1]:
+            self.y += 0.01
 
-            self.mainarm_msg.handtheta -= self.joy_msg.axes[Axes.RX]/100
-            if self.mainarm_msg.handtheta < -math.pi/2:
-                self.mainarm_msg.handtheta = -math.pi/2
-            elif self.mainarm_msg.handtheta > math.pi/2:
-                self.mainarm_msg.handtheta = math.pi/2
+        if self.joy_msg.buttons[2]:
+            self.y += 0.005
+        
+        if self.joy_msg.buttons[3]:
+            self.y -= 0.005
+        
+        if self.joy_msg.buttons[4]:
+            self.y -= 0.01
 
-            self.cartesian_xy[0] = self.mainarm_msg.r * \
-                math.cos(self.mainarm_msg.theta)
-            self.cartesian_xy[1] = self.mainarm_msg.r * \
-                math.sin(self.mainarm_msg.theta)
-            self.get_logger().info('x: %f, y: %f' %
-                                   (self.cartesian_xy[0], self.cartesian_xy[1]))
+        if self.joy_msg.buttons[5]:
+            self.y -= 0.05
 
-        elif self.coordinate_mode == CoordinateMode.CARTESIAN:
-            self.cartesian_xy[0] -= self.joy_msg.axes[Axes.LX]/100
-            self.cartesian_xy[1] += self.joy_msg.axes[Axes.LY]/100
+        if self.joy_msg.buttons[6]:
+            self.seiton_msg.mode = YURAYURA
 
-            self.mainarm_msg.r = math.sqrt(
-                self.cartesian_xy[0]**2 + self.cartesian_xy[1]**2)
-            if self.mainarm_msg.r < 0.0:
-                self.mainarm_msg.r = 0.0
-            elif self.mainarm_msg.r > 1.0:
-                self.mainarm_msg.r = 1.0
+        if self.joy_msg.buttons[7]:
+            self.seiton_msg.mode = GURIGURI
 
-            self.mainarm_msg.theta = math.atan2(self.cartesian_xy[1], self.cartesian_xy[0]) if not -math.pi/2 > math.atan2(
-                self.cartesian_xy[1], self.cartesian_xy[0]) > -math.pi else math.atan2(self.cartesian_xy[1], self.cartesian_xy[0]) + 2*math.pi
-            if self.mainarm_msg.theta < -math.pi/2:
-                self.mainarm_msg.theta = -math.pi/2
-            elif self.mainarm_msg.theta > math.pi*3/2:
-                self.mainarm_msg.theta = math.pi*3/2
-            self.mainarm_msg.handtheta = 1.57-self.mainarm_msg.theta
+        if self.joy_msg.buttons[8]:
+            self.seiton_msg.mode = PATAPATA
 
-            self.get_logger().info('x: %f, y: %f' %
-                                   (self.cartesian_xy[0], self.cartesian_xy[1]))
+        if self.joy_msg.buttons[9]:
+            self.seiton_msg.flip = True
 
-        self.mainarm_msg.lift -= (1 - (self.joy_msg.axes[Axes.RT]+1)/2)/100
-        if self.mainarm_msg.lift < 0.0:
-            self.mainarm_msg.lift = 0.0
-
-        self.mainarm_msg.lift += (1 - (self.joy_msg.axes[Axes.LT]+1)/2)/100
-        if self.mainarm_msg.lift > 1.0:
-            self.mainarm_msg.lift = 1.0
-
-        if self.joy_msg.buttons[Buttons.LSTICK] == 1:
-            self.mainarm_msg.handtheta = 1.57-self.mainarm_msg.theta
-
-        if self.joy_msg.buttons[Buttons.B]:
-            self.mainarm_msg.hand = True
-
-        if self.joy_msg.buttons[Buttons.A]:
-            self.mainarm_msg.hand = False
-
-        if self.joy_msg.buttons[Buttons.X]:
-            self.mainarm_msg.roller = True
-
-        if self.joy_msg.buttons[Buttons.Y]:
-            self.mainarm_msg.roller = False
-
-        if self.joy_msg.buttons[Buttons.HOME] != self.previous_joy_msg.buttons[Buttons.HOME] and self.joy_msg.buttons[Buttons.HOME]:
-            self.coordinate_mode = not self.coordinate_mode
+        if self.joy_msg.buttons[10]:
+            self.seiton_msg.flip = False
+        
+        if self.joy_msg.buttons[11]:
+            self.seiton_msg.conveyer += 1
+        
+        if self.joy_msg.buttons[12]:
+            self.seiton_msg.conveyer -= 1
 
         self.pose_pub.publish(self.mainarm_msg)
         self.previous_joy_msg = self.joy_msg
